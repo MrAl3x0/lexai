35527cfacd7685889f47db458345c0a0ce53fbeb
Alex Ulanch <alexulanch@gmail.com>
2023-04-04 19:41:51 -0700

    update README

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ model_engine = "text-embedding-ada-002"
 location_info = {
     "Boulder": {
         "npz_file": "embeddings/boulder_embeddings.npz",
-        "role_description": "You are an AI-powered legal assistant specializing in the jurisdiction of Boulder County, Colorado."
+        "role_description": "You are an AI-powered legal assistant specializing in the jurisdiction of Boulder County, Colorado.",
     },
     "Denver": {
         "npz_file": "embeddings/denver_embeddings.npz",
-        "role_description": "You are an AI-powered legal assistant specializing in the jurisdiction of Denver, Colorado."
-    }
+        "role_description": "You are an AI-powered legal assistant specializing in the jurisdiction of Denver, Colorado.",
+    },
 }
 
 
@@ -46,39 +46,38 @@ def generate_matches(query, location, api_key):
     """
     try:
         openai.api_key = api_key
-        query_embedding = openai.Embedding.create(
-            input=query,
-            model=model_engine
-        )
+        query_embedding = openai.Embedding.create(input=query, model=model_engine)
 
         query_embedding_json = query_embedding.to_dict()
-        query_embedding = np.array(
-            query_embedding_json['data'][0]['embedding'])
+        query_embedding = np.array(query_embedding_json["data"][0]["embedding"])
 
         # Determine the .npz file and role description to use based on the user-selected location
         location_data = location_info.get(location)
         if not location_data:
             raise Exception(f"No data found for location '{location}'.")
 
-        npz_file = location_data['npz_file']
-        role_description = location_data['role_description']
+        npz_file = location_data["npz_file"]
+        role_description = location_data["role_description"]
 
         data = np.load(npz_file, allow_pickle=True)
-        embeddings = data['embeddings']
+        embeddings = data["embeddings"]
 
-        jurisdiction_data = pd.DataFrame({
-            'url': data['urls'],
-            'title': data['titles'],
-            'subtitle': data['subtitles'],
-            'content': data['contents']
-        })
+        jurisdiction_data = pd.DataFrame(
+            {
+                "url": data["urls"],
+                "title": data["titles"],
+                "subtitle": data["subtitles"],
+                "content": data["contents"],
+            }
+        )
 
-        distances = cdist(query_embedding.reshape(1, -1),
-                          embeddings, metric='cosine')[0]
+        distances = cdist(query_embedding.reshape(1, -1), embeddings, metric="cosine")[
+            0
+        ]
         indices = np.argsort(distances)[:3]
-        top_matches = jurisdiction_data.iloc[indices].to_dict('records')
+        top_matches = jurisdiction_data.iloc[indices].to_dict("records")
 
-        role = f'''
+        role = f"""
           {role_description} 
           Your expertise lies in providing accurate and timely information on the laws and regulations specific to your jurisdiction.
 
@@ -86,7 +85,7 @@ def generate_matches(query, location, api_key):
           in understanding and applying legal standards within this jurisdiction. You are knowledgeable, precise, and always 
           ready to offer guidance on legal matters. Your max_tokens is set to 120 so keep your
           response below that.
-          '''
+          """
 
         response = openai.ChatCompletion.create(
             model="gpt-4",
@@ -100,7 +99,7 @@ def generate_matches(query, location, api_key):
             max_tokens=120,
             top_p=1,
             frequency_penalty=0,
-            presence_penalty=0
+            presence_penalty=0,
         )
         message = response["choices"][0]["message"]["content"].strip()
         html_response = "<p><strong>Response:</strong></p><p>" + message + "</p>"
@@ -112,21 +111,47 @@ def generate_matches(query, location, api_key):
         return html_response + html_references
 
     except Exception as e:
-        html_message = '''<p style="font-family: Arial, sans-serif; font-size: 16px; color: #333;">
+        html_message = """<p style="font-family: Arial, sans-serif; font-size: 16px; color: #333;">
     <strong>Notice:</strong> An error occurred while processing your request. Please see the details below:
-</p>'''
+</p>"""
         return f"<p>{html_message} {str(e)}</p>"
 
 
-description = "LegalLens is an AI-powered legal research app designed to assist individuals, including law enforcement officers, legal professionals, and the general public, in accessing accurate legal information. The app covers various jurisdictions and ensures that users can stay informed and confident, regardless of their location. This demo is meant to serve as a proof of concept."
+description = "LexAI is an AI-powered legal research app designed to assist individuals, including law enforcement officers, legal professionals, and the general public, in accessing accurate legal information. The app covers various jurisdictions and ensures that users can stay informed and confident, regardless of their location. This demo is meant to serve as a proof of concept."
 
-iface = gr.Interface(title="LegalLens Demo", description=description, fn=generate_matches, inputs=[gr.Textbox(label="Query"), gr.Dropdown(choices=["Boulder", "Denver"], label="Location"), gr.Textbox(label="OpenAI API key", placeholder="must have access to GPT-4")], outputs=["html"], examples=[
-    ["Is it legal for me to use rocks to construct a cairn in an outdoor area?", "Boulder"],
-    ["Is it legal to possess a dog and take ownership of it as a pet in Boulder?", "Boulder"],
-    ["As per the local laws, am I allowed to expose my upper body and go without a shirt in public places?", "Boulder"],
-    ["What are the legal restrictions regarding the maximum height of a building that can be constructed in a particular area?", "Boulder"],
-    ["Is it legal to place a couch on my porch according to the local regulations?", "Boulder"],
-    ["Can I legally graze my llamma on public land?", "Boulder"]
-])
+iface = gr.Interface(
+    title="LexAI Demo",
+    description=description,
+    fn=generate_matches,
+    inputs=[
+        gr.Textbox(label="Query"),
+        gr.Dropdown(choices=["Boulder", "Denver"], label="Location"),
+        gr.Textbox(label="OpenAI API key", placeholder="must have access to GPT-4"),
+    ],
+    outputs=["html"],
+    examples=[
+        [
+            "Is it legal for me to use rocks to construct a cairn in an outdoor area?",
+            "Boulder",
+        ],
+        [
+            "Is it legal to possess a dog and take ownership of it as a pet in Boulder?",
+            "Boulder",
+        ],
+        [
+            "As per the local laws, am I allowed to expose my upper body and go without a shirt in public places?",
+            "Boulder",
+        ],
+        [
+            "What are the legal restrictions regarding the maximum height of a building that can be constructed in a particular area?",
+            "Boulder",
+        ],
+        [
+            "Is it legal to place a couch on my porch according to the local regulations?",
+            "Boulder",
+        ],
+        ["Can I legally graze my llamma on public land?", "Boulder"],
+    ],
+)
 
 iface.launch()
